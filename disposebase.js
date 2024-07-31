3cbfd3c701c4831614d325c0d43ad968aa7fc40d
since <whenevertobefc@outlook.com>
2024-07-31 14:51:56 +0800

    update refer

--- disposebase.js
+++ disposebase.js
@@ -18,7 +18,7 @@ arr.forEach((item, index) => {
     id: index + 2,
     name: item.title,
     // 个人分销码粘贴替换在此处
-    url: `${item.url}?refer=1fa1da45-327e-4651-b13c-c73d21cc297f`,
+    url: `${item.url}?refer=fa4b95f2-5377-4703-b4d3-95b0951a272d`,
     // url: `${item.url}?refer=fa4b95f2-5377-4703-b4d3-95b0951a272d`,
     desc: item.introduction,
     favicon: `/imgNew/image_${index}.jpg`,
