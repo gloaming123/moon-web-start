ca888591a738270ea77c82be7ebac4af79f3069e
gloaming <1310528998@qq.com>
2024-08-13 15:10:57 +0800

    删除搜索栏

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import MainHeader from './components/MainHeader.vue'
 import MainClock from './components/MainClock.vue'
-import MainSearch from './components/MainSearch.vue'
 import SiteContainer from './components/SiteContainer.vue'
 import MainSetting from './components/MainSetting.vue'
 
@@ -17,7 +16,7 @@ const settingStore = useSettingStore()
     <div my="0 sm:6vh" p="12 sm:24" bg="$main-bg-c" w="full sm:auto" :class="{ no_select: settingStore.isSetting }">
       <MainHeader />
       <MainClock v-if="!settingStore.isSetting" />
-      <MainSearch v-if="!settingStore.isSetting" my-24 />
+      <!-- <MainSearch v-if="!settingStore.isSetting" my-24 /> -->
       <SiteContainer :key="settingStore.siteContainerKey" />
       <MainSetting />
       <TheFooter v-if="settingStore.getSettingValue('showFooter')" />
